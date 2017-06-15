86638a9be727c35aa262eedef3769164878cbdd8
Bruno Gomes Coelho <brasinhacoelho@hotmail.com>
2017-06-14 22:15:16 -0300

    Create SpacialTrace.py

---
+++ SpacialTrace.py
@@ -0,0 +1,31 @@
+from random import randint, random 
+# randint: retorna um número inteiro aleatorio (a, b) tq a <= N <= b
+# random: numero aleatorio entre 0 e 1
+
+tamMemoria = 100
+tamBloco = 4
+
+qtdTraces = 1000
+
+freqLeitura = 0.68
+freqEscrita = 1 - freqLeitura
+threshold = 2
+
+rotLeitura = 2
+rotEscrita = 1
+
+pos = randint(0, tamMemoria-1)
+
+
+for i in range(qtdTraces):
+    rand = random();
+    if (rand <= freqLeitura):
+        print(rotLeitura, end = " ")
+    else:
+        print(rotEscrita, end = " ")
+    
+    pos += randint(-threshold*tamBloco, threshold*tamBloco)
+    
+    if (pos < 0 or pos > tamMemoria-1):
+        pos = randint(0, tamMemoria-1)
+    print(pos)
